73ec179f98f41bb6041def50398e8267978147fe
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-26 16:17:55 +0300

    ViewPager2, TabLayout

--- tablayout/src/main/java/com/pk4us/tablayout/MainActivity.kt
+++ tablayout/src/main/java/com/pk4us/tablayout/MainActivity.kt
@@ -2,24 +2,25 @@ package com.pk4us.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TableLayout
-import android.widget.Toast
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.pk4us.tablayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val list = listOf(Fragment1.newInstance(),Fragment2.newInstance(),Fragment3.newInstance())
+    private val listTitles = listOf("item 1","item 2","item 3",)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tb.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                supportFragmentManager.beginTransaction().replace(R.id.placeHolder,list[tab?.position!!]).commit()
-            }
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
+
+        val adapter = VpAdapter(this,list)
+        binding.vp2.adapter = adapter
+
+        TabLayoutMediator(binding.tb,binding.vp2){
+            tab,pos -> tab.text = listTitles[pos]
+        }.attach()
     }
 }
